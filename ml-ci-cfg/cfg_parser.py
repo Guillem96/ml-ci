1b083fe5a7134abdb00c18a1b8ce850bdaae260a
Guillem Orellana <guillem.orellana@gmail.com>
2019-01-07 19:03:47 +0100

    First version of configure file parser

---
+++ ml-ci-cfg/cfg_parser.py
@@ -0,0 +1,50 @@
+import yaml
+
+class ModelCfg(object):
+
+    def __init__(self, name, params, fine_tune):
+        self.name = name
+        self.params = params
+        self.fine_tune = fine_tune
+
+    def __str__(self):
+        return '{ ' + 'Algorithm: {}, Params: {}, Fine Tune: {}' \
+                            .format(self.name, self.params, self.fine_tune) + ' }'
+
+class MlCiCfg(object):
+
+    def __init__(self, **kwargs):
+        self.test_rate = kwargs.get('test_rate')
+        self.models = kwargs.get('models')
+        self.pipeline = kwargs.get('pipeline')
+
+    def __str__(self):
+        return 'Test-rate: {}\nPipeline: {}\nModels: {}' \
+                        .format(self.test_rate, self.pipeline, self._models_str())
+
+    def _models_str(self):
+        return '[\n  ' + ',\n  '.join(map(str, self.models)) + '\n]'
+
+
+class YamlParser(object):
+
+    def __init__(self, yaml_stream):
+        cfg_dict = yaml.load(yaml_stream)
+        config = dict()
+        config['test_rate'] = cfg_dict.get('test-rate', 0.2)
+        config['pipeline'] = cfg_dict.get('pipeline')
+        config['models'] = YamlParser._parse_models(cfg_dict)
+        self.parsed_cfg = MlCiCfg(**config)
+
+
+    @staticmethod
+    def _parse_models(cfg_dict):
+        models = cfg_dict.get('models', [])
+        return [ ModelCfg(model, models.get('params'), models.get('fine-tune', False)) 
+                            for model in models.keys() ]
+
+
+
+if __name__ == '__main__':
+    with open('ml-ci.yml', 'r') as f:
+        print(str(YamlParser(f).parsed_cfg))
